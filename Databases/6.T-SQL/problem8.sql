846a95ffc8d7bf60204f6458caa0b31df1cd434b
Presian <presiandanailov@gmail.com>
2015-02-19 11:01:26 +0200

    Little optimize!

--- Databases/6.T-SQL/problem8.sql
+++ Databases/6.T-SQL/problem8.sql
@@ -16,26 +16,51 @@ INNER JOIN Towns t
 	ON t.TownID = a.TownID
 
 OPEN empCursor
-DECLARE @firstName NVARCHAR(50), 
-@lastName NVARCHAR(50), 
-@town NVARCHAR(50) = '',
-@fullName NVARCHAR(50) = '',
-@currentTown NVARCHAR(50) = ''
-FETCH NEXT FROM empCursor INTO @firstName, @lastName,  @town
+DECLARE @firstName NVARCHAR(MAX), 
+@lastName NVARCHAR(MAX), 
+@town NVARCHAR(MAX) = '',
+@currentTown NVARCHAR(MAX) = ''
+FETCH NEXT FROM empCursor INTO 
+	@firstName, 
+	@lastName,  
+	@town
 WHILE @@FETCH_STATUS = 0
   BEGIN
-	if @fullName = ''
-	BEGIN
-	SET @fullName = @firstName + ' ' + @lastName
-	END
-	SET @currentTown = @town
-    PRINT @lastName + ': ' + @fullName + ' ' + @town + ' ' + @firstName
-    FETCH NEXT FROM empCursor 
-    INTO @firstName, @lastName, @town
-	IF @currentTown <> @town
-	BEGIN
-		SET @fullName = ''
-	END
+	DECLARE innerEmpCursor CURSOR READ_ONLY FOR
+	SELECT e.FirstName, e.LastName, t.Name
+	FROM Employees e
+	INNER JOIN Addresses a
+		ON a.AddressID = e.AddressID
+	INNER JOIN Towns t
+		ON a.TownID = t.TownID
+	WHERE t.Name = @town
+	ORDER BY a.AddressID
+	OPEN innerEmpCursor
+	DECLARE @innerFirstName NVARCHAR(MAX), 
+	@innerLastName NVARCHAR(MAX),
+	@innerTown NVARCHAR(MAX)
+	FETCH NEXT FROM innerEmpCursor INTO 
+		@innerFirstName, 
+		@innerLastName, 
+		@innerTown
+		WHILE @@FETCH_STATUS = 0
+			BEGIN
+			PRINT @innerLastName + ': ' 
+				+ @firstName + ' ' 
+				+ @lastName + ' ' 
+				+ @town + ' ' 
+				+ @innerLastName
+			FETCH NEXT FROM innerEmpCursor 
+			INTO @innerLastName, 
+				@innerLastName, 
+				@innerTown
+		END
+	CLOSE innerEmpCursor
+	DEALLOCATE innerEmpCursor
+	FETCH NEXT FROM empCursor 
+	INTO @firstName, 
+		@lastName, 
+		@town
   END
 CLOSE empCursor
 DEALLOCATE empCursor
